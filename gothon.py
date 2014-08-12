e58dd4a8405b4323a1daf9bbb211410b8e6db1e5
BHI air <bhiair@BHI-no-MacBook-Air.local>
2014-08-12 15:48:33 +0900

    2nd revision

--- gothon.py
+++ gothon.py
@@ -35,3 +35,28 @@ class Death(Scene):
 		print Death.quips[randint(0, len(self.quips)-1)]
 		exit(1)
 		
+class CentralCorridor(Scene):
+
+	def enter(self):
+		print "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
+		print "your entire crew. You are the last surviving member and your last"
+		print "mission is to get the neutron destruct bomb from the Weapons Armory,"
+		print "put it in the bridge, and blow the ship up after getting into an "
+		print "escape pod."
+		print "\n"
+		"You're running down the central corridor to the Weapons Armory when"
+		print "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
+		print "flowing around his hate filled body. He's blocking the door to the"
+		print "Armory and about to pull a weapon to blast you."
+		
+		action = raw_input("> ")
+		
+		if action == "shoot!":
+			print "Quick on the draw you yan out your blaster and fire it at the Gothon."
+			print "His clown costume is flowing and moving around his body, which throws"
+			print "off your aim. Your laster hits his costume but misses him entirely. This"
+			print "completely ruins his brand new costume his mother bought him, which"
+			print "makes him fly into a rage and blast you repeatedly in the face until"
+			print "you are dead. Then he eats you."
+			return 'death'
+		
